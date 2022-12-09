8ad24c7f14bd3ab3d82eb5f1e75d112e64ab6330
Joaovitorvas <joaovitorvasc.2307@outlook.com>
2022-12-09 18:19:37 -0300

    Botão feito

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,16 @@
-import { ButtonContainer } from "./styles";
+import { ButtonContainer, ButtonContainerFalse } from "./styles";
 import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+const Button = ({ title, onClick, disabled }: IButtonProps) => {
+  if(disabled === false){
+    return <ButtonContainer onClick={onClick} disabled={disabled}>
+    {title}
+    </ButtonContainer>;
+  } else {
+    return <ButtonContainerFalse onClick={onClick} disabled={disabled}>
+    {title}
+    </ButtonContainerFalse>;
+  }
 };
 
 export default Button;
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -15,3 +15,13 @@ export const ButtonContainer = styled.button`
         cursor:pointer;
     }
 `
+
+export const ButtonContainerFalse = styled.button`
+    width: 100%;
+    height: 42px;
+    background-color: #81259D;
+    color: #FFF;
+
+    border: 1px solid #81259D;
+    border-radius: 21px;
+`
